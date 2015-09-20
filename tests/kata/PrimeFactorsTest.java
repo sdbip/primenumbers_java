b0d7283abd423c5739c2d557eba0f06fab823311
Johan Segerfeldt <johan.segerfeldt@me.com>
2015-09-20 22:09:07 +0200

    Second test

--- tests/kata/PrimeFactorsTest.java
+++ tests/kata/PrimeFactorsTest.java
@@ -3,6 +3,7 @@ package kata;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 import static org.junit.Assert.assertEquals;
 
@@ -11,6 +12,11 @@ public class PrimeFactorsTest {
 	@Test
 	public void generate_one_returnsEmptyList() {
 		assertEquals(new ArrayList<Integer>(), PrimeFactors.generate(1));
+	}
+
+	@Test
+	public void generate_two_returnsTwo() {
+		assertEquals(Collections.singletonList(2), PrimeFactors.generate(2));
 	}
 
 }
